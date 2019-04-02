e95c94692a975e64ba185aa0f23dda7a044c8fee
qwertyER000 <47403826+qwertyER000@users.noreply.github.com>
2019-04-02 20:21:56 +0200

    Update ImageMapping.cpp

--- ImageMapping.cpp
+++ ImageMapping.cpp
@@ -1,5 +1,10 @@
 #include <ogx/Plugins/EasyPlugin.h>
+#include <ogx/Math/Calculation.h>
+#include <ogx/Math/Optimization.h>
 #include <ogx/Data/Clouds/CloudHelpers.h>
+#include <ogx/Data/Clouds/SearchKernels.h>
+
+
 #include"CameraCV.h"
 #include "ColorMapping.h"
 
@@ -7,18 +12,19 @@
 using namespace ogx;
 using namespace ogx::Data;
 
-struct ImageMapping : public ogx::Plugin::EasyMethod
+struct ImageMapping : public ogx::Plugin::EasyMethod, ogx::Math::ModelI
 {
 	//// parameters
 	Data::ResourceID node_id;
 	String img_path;
-	int height, width, with_new_fram;
-	cv::Mat silhuette2D, silhuette3D;
+	int height, width, with_new_fram, m_far;
+	cv::Mat silhuette2D;
 	std::vector<cv::Point3f> CloudXYZ;
-	Context n_context;
+	cv::Mat camera_matrix, translation, rotation;
+	cv::Mat solver_rot;
 
 	// constructor
-	ImageMapping() : EasyMethod(L"Ewa Rycaj", L"cos.")
+	ImageMapping() : EasyMethod(L"Ewa Rycaj", L"Image to geometry registration")
 	{
 	}
 
@@ -56,7 +62,7 @@ struct ImageMapping : public ogx::Plugin::EasyMethod
 	}
 
 	//silhouette from camera image
-	void silhouetteFromImage(cv::Mat &img_in, cv::Mat &img_out)
+	virtual void silhouetteFromImage(cv::Mat &img_in, cv::Mat &img_out)
 	{
 		cv::Mat thres;
 
@@ -76,68 +82,28 @@ struct ImageMapping : public ogx::Plugin::EasyMethod
 		thres.release();
 	}
 
-	double estimateCamera(std::vector<float> params)
-	{
-		//cv::Mat intrinsic_matrix = cv::Mat::zeros(cv::Size(3, 3), CV_32FC1); //camera matrix
-		cv::Mat tvec = cv::Mat::zeros(cv::Size(1, 3), CV_32FC1); // translation vector
-		cv::Mat rvec = cv::Mat::zeros(cv::Size(1, 3), CV_32FC1); //rotation vector
-		cv::Mat framSilhouette = cv::Mat::zeros(cv::Size(with_new_fram, height), CV_8UC1);
-		cv::Mat XOR;
-
-
-		rvec.at<float>(0, 0) = params[1];
-		rvec.at<float>(1, 0) = params[2];
-		rvec.at<float>(2, 0) = params[3];
-		tvec.at<float>(0, 0) = params[4];
-		tvec.at<float>(1, 0) = params[5];
-		tvec.at<float>(2, 0) = params[6];
-
-		CVCamera cam;
-		cam.updateView(n_context, params[0], tvec, rvec, height);
-		auto matrix = cam.camera_matrix(n_context.Feedback().GetCamera().GetProjection().GetMatrix(), with_new_fram, height);
-		matrix.at<float>(0, 0) = params[0];
-		matrix.at<float>(1, 1) = params[0];
-
-		renderSilhouette render3d;
-		render3d.GetSilhouette(rvec, tvec, matrix, CloudXYZ, framSilhouette);
-		cv::Rect ROI(framSilhouette.cols / 2 - width / 2, 0, width, height);
-		cv::Mat img3D = framSilhouette(ROI);
-		silhuette3D = render3d.RemoveHoles(img3D);
-
-		render3d.similarityImg(silhuette3D, silhuette2D, XOR);
-
-		float misalignment = cv::countNonZero(XOR)/(XOR.rows*XOR.cols);
-
-		return misalignment;
-	}
-
 
 	virtual void Run(Context& context)
 	{
 		auto subtree = context.Project().TransTreeFindNode(node_id);
 		// report error if given node was not found, this will stop execution of algorithm
 		if (!subtree) ReportError(L"Node not found");
+		//camera_matrix = cv::Mat::zeros(cv::Size(3, 3), CV_32FC1);
 		
 		//CAMERA
 		with_new_fram = height * context.Feedback().GetCamera().GetProjection().GetAspect();
 		CVCamera camera;
-		cv::Mat viewMatrix = camera.camera_matrix(context.Feedback().GetCamera().GetProjection().GetMatrix(), with_new_fram, height); //camera matrix
-		cv::Mat tvec = camera.tvec(context.Feedback().GetCamera().GetViewTransform()); // translation vector
-		cv::Mat rvec = camera.rvec(context.Feedback().GetCamera().GetViewTransform()); //rotation vector
+		camera_matrix = camera.camera_matrix(context.Feedback().GetCamera().GetProjection().GetMatrix(), with_new_fram, height); //camera matrix
+		float f = camera_matrix.at<float>(1, 1);
+		translation = camera.tvec(context.Feedback().GetCamera().GetViewTransform()); // translation vector
+		rotation = camera.rvec(context.Feedback().GetCamera().GetViewTransform()); //rotation vector
 		auto camera_position = context.Feedback().GetCamera().GetPosition(); //get current camera position
+		int far_pl = context.Feedback().GetCamera().GetProjection().GetFarClippingPlane();
+
+		//camera.updateView(context, viewMatrix.at<float>(1, 1), tvec, rvec, height);
 		
-																			 //double height_fram = context.Feedback().GetViewportRect().h;
-		//OPTIMIZATION PARAMS
-		std::vector<float> parameters = { 0, 0, 0, 0, 0, 0, 0 };
-		parameters[0] = viewMatrix.at<float>(1, 1); //focal length
-		//rotation
-		parameters[1] = rvec.at<float>(0, 0);
-		parameters[2] = rvec.at<float>(1, 0);
-		parameters[3] = rvec.at<float>(2, 0);
-		//translation
-		parameters[4] = tvec.at<float>(0, 0);
-		parameters[5] = tvec.at<float>(1, 0);
-		parameters[6] = tvec.at<float>(2, 0);
+		 //double height_fram = context.Feedback().GetViewportRect().h;
+
 
 		//TEXTURE IMG
 		cv::Mat imgCam;
@@ -145,6 +111,7 @@ struct ImageMapping : public ogx::Plugin::EasyMethod
 		silhouetteFromImage(imgCam, silhuette2D);
 		width = silhuette2D.cols;
 
+		
 		//3D MODEL TO CV
 		renderSilhouette obj;
 		// perform calculation for each cloud
@@ -158,10 +125,86 @@ struct ImageMapping : public ogx::Plugin::EasyMethod
 
 			obj.pointsToCV_all(points, CloudXYZ);
 		}, thread_count);
-		
-		estimateCamera(parameters);
+
+		auto solver = Math::SolverI::Create();
+		solver->Solve(*this);
+
+		//cv::Mat rvec = cv::Mat::zeros(cv::Size(1, 3), CV_32FC1); //rotation vector
+		camera.updateView(context, f, translation, rotation, height);
+		delete solver;
 	}
 
+	virtual Count GetResultCount()
+	{
+		return 1;
+	}
+
+	virtual Count GetParamCount()
+	{
+		return 3;
+	}
+
+	virtual void InitParams(Math::Vector& params)
+	{
+
+		//params[0] = camera_matrix.at<float>(1, 1)/m_far*1000;
+
+		params[0] = rotation.at<float>(0, 0);
+		params[1] = rotation.at<float>(1, 0);
+		params[2] = rotation.at<float>(2, 0);
+		//params[4] = translation.at<float>(0, 0);
+		//params[5] = translation.at<float>(1, 0);
+		//params[6] = translation.at<float>(2, 0);
+	}
+
+	virtual bool Iterate(const Math::Vector& params, Math::Vector& results)
+	{
+		//cv::Mat tvec = cv::Mat::zeros(cv::Size(1, 3), CV_32FC1); // translation vector
+		//cv::Mat rvec = cv::Mat::zeros(cv::Size(1, 3), CV_32FC1); //rotation vector
+		//cv::Mat matrix = cv::Mat::zeros(cv::Size(1, 3), CV_32FC1); 
+
+		cv::Mat framSilhouette = cv::Mat::zeros(cv::Size(with_new_fram, height), CV_8UC1);
+		cv::Mat XOR;
+
+		//camera_matrix.at<float>(0, 0) = -params(0)*m_far/1000;
+		//camera_matrix.at<float>(1, 1) = params(0)*m_far/1000;
+		rotation.at<float>(0, 0) = params[0];
+		rotation.at<float>(1, 0) = params[1];
+		rotation.at<float>(2, 0) = params[2];
+		//tvec.at<float>(0, 0) = params[4];
+		//tvec.at<float>(1, 0) = params[5];
+		//tvec.at<float>(2, 0) = params[6];
+
+
+		renderSilhouette render3d;
+		render3d.GetSilhouette(rotation, translation, camera_matrix, CloudXYZ, framSilhouette);
+		cv::Rect ROI(framSilhouette.cols / 2 - width / 2, 0, width, height);
+		cv::Mat img3D = framSilhouette(ROI);
+		cv::Mat silhuette3D = render3d.RemoveHoles(img3D);
+
+		render3d.similarityImg(silhuette3D, silhuette2D, XOR);
+
+		double pixels_all = XOR.rows*XOR.cols;
+		
+		results(0) = (double)cv::countNonZero(XOR) / pixels_all;
+
+		OGX_LINE.Format(ogx::Info, L"Misalignment error", results);
+
+		//tvec.release();
+		//rvec.release();
+		img3D.release();
+		silhuette3D.release();
+		XOR.release();
+
+		return true;
+	}
+
+	virtual void SaveSolution(const Math::Vector& params)
+	{
+		OGX_LINE.Format(ogx::Debug, L"Current solver solution is: %f, %f, %f, %f, %f, %f", params(0));
+		//OGX_LINE.Format(ogx::Debug, L"Current solver solution is: %f, %f, %f, %f, %f, %f", params[0], params[1], params[2], params[3], params[4], params[5], params[6]);
+
+	}
 
 };
 
